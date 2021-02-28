3a6086edbdde06c4da50c65c01f36392b0f0045c
Damon Bauer <dbauer@planview.com>
2021-02-27 20:06:53 -0600

    Updates dependencies

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const path = require('path');
 const macosVersion = require('macos-version');
 const execa = require('execa');
+const electronUtil = require('electron-util/node');
 
-const bin = path.join(__dirname, 'main');
+const binary = path.join(electronUtil.fixPathForAsarUnpack(__dirname), 'main');
 
 module.exports = async () => {
 	macosVersion.assertGreaterThanOrEqualTo('10.11');
-	return (await execa.stdout(bin)) === 'true';
+
+	const {stdout} = await execa(binary, ['status']);
+	return stdout === 'true';
 };
